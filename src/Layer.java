6dfe4183c5e463ad3e482c30e98d0cdb07882c66
Daniel Church <daniellchurch@fvcc.edu>
2017-10-04 13:46:21 -0600

    Added a few functions

--- src/Layer.java
+++ src/Layer.java
@@ -1,14 +1,25 @@
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Layer {
 
     private Type layerType;
-    public List<Node> nodes;
+    public Node[] nodes;
     public int layerCount;
 
-    public Layer(int nodes, Type layerType) {}
+    public Layer(int nodeCount, Type layerType) {
+        nodes = new Node[nodeCount];
 
-    public void updateNodeWeights(List<List<Double>> weights){}
-    public List<Double> calculateNodeOutputs(){return null;}
+        Stream.iterate(0, i -> i + 1).forEach(i -> nodes[i] = new Node(layerType));
+    }
+
+    public void updateNodeWeights(List<List<Double>> weights){
+        Stream.iterate(0, i -> i + 1).forEach(i -> nodes[i].updateWeights(weights.get(i)));
+    }
+
+    public List<List<Double>> calculateNodeOutputs(){
+        return Stream.of(nodes).map(n -> n.calculateOutputs()).collect(Collectors.toList());
+    }
 
 }
