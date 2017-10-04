e240e3a7c60a0f70162efe8d593f4736f405ddf5
Daniel Church <daniellchurch@fvcc.edu>
2017-10-04 12:00:34 -0600

    Update Network.java
    What have I done..

--- src/Network.java
+++ src/Network.java
@@ -7,11 +7,11 @@ public class Network {
 
     public Network(int hiddenLayers, int nodesPerHiddenLayer, int inputCount, int dimension, boolean isRadialBases) {}
 
-    public void forwardPropogate(){};
-    public void backPropogate(){};
-    public List<Double> calculateError(){return null;};
-    private void kMeansCluster(int k){};
-    private double calculateSigma(){return 0d;};
-    private double rossenBrock(double ... values){return 0d;};
+    public void forwardPropogate(){}
+    public void backPropogate(){}
+    public List<Double> calculateError(){return null;}
+    private void kMeansCluster(int k){}
+    private double calculateSigma(){return 0d;}
+    private double rossenBrock(double ... values){return 0d;}
 
 }
