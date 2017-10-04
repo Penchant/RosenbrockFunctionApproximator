494503ec95e890afecc874a4c7cfc82a6150fc27
Daniel Church <daniellchurch@fvcc.edu>
2017-10-04 13:46:21 -0600

    Started implementing the functions

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Node {
 
@@ -7,7 +11,46 @@ public class Node {
     public List<Double> inputs;
     public List<Double> weights;
 
-    public double calculateOutputs(){return 0d;}
-    public void updateWeights(List<Double> weightChanges){}
+    public Node(Type nodeType) {
+        this.nodeType = nodeType;
+    }
+
+    public List<Double> calculateOutputs() {
+        final Function<Double, Double> activationFunction;
+
+        switch(nodeType) {
+            case HIDDEN:    activationFunction = this::logisticActivation; break;
+            case RBFHIDDEN: activationFunction = this::logisticActivation; break;
+            case INPUT:     activationFunction = this::linearActivation;   break;
+            case OUTPUT:    activationFunction = this::linearActivation;   break;
+            case RBFINPUT:  activationFunction = this::linearActivation;   break;
+            default:        activationFunction = this::linearActivation;   break;
+        }
+
+        return Stream.iterate(0, i -> i + 1)
+                .limit(inputs.size())
+                .map(i -> calculateOutput(activationFunction.apply(inputs.get(i)), weights.get(i)))
+                .collect(Collectors.toList());
+    }
+
+    public void updateWeights(List<Double> newWeights) {
+        weights = newWeights;
+    }
+
+    private double gaussianBasisFunction(double value, double musubj, double delta) {
+        return Math.pow(Math.E, - Math.pow(value - musubj, 2) / (2 * delta * delta));
+    }
+
+    private double calculateOutput(double value, double weight) {
+        return value * weight;
+    }
+
+    private double linearActivation(double value) {
+        return value;
+    }
+
+    private double logisticActivation(double value) {
+        return 1d / (1 + Math.pow(Math.E, -value));
+    }
 
 }
