04cf959d92c61381319a0f1dd19f9c7ee28e32d8
danielchurch <daniellchurch@fvcc.edu>
2017-10-05 00:06:39 -0600

    Java 8 magic

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -19,17 +18,18 @@ public class Node {
         final Function<Double, Double> activationFunction;
 
         switch(nodeType) {
-            case HIDDEN:    activationFunction = this::logisticActivation; break;
-            case RBFHIDDEN: activationFunction = this::logisticActivation; break;
-            case INPUT:     activationFunction = this::linearActivation;   break;
-            case OUTPUT:    activationFunction = this::linearActivation;   break;
-            case RBFINPUT:  activationFunction = this::linearActivation;   break;
-            default:        activationFunction = this::linearActivation;   break;
+            case HIDDEN:    activationFunction = logisticActivation; break;
+            case RBFHIDDEN: activationFunction = logisticActivation; break;
+            case INPUT:     activationFunction = linearActivation;   break;
+            case OUTPUT:    activationFunction = linearActivation;   break;
+            case RBFINPUT:  activationFunction = linearActivation;   break;
+            default:        activationFunction = linearActivation;   break;
         }
 
         return Stream.iterate(0, i -> i + 1)
                 .limit(inputs.size())
-                .map(i -> calculateOutput(activationFunction.apply(inputs.get(i)), weights.get(i)))
+                .map(i -> new Double[]{activationFunction.apply(inputs.get(i)), weights.get(i)})
+                .map(calculateOutput)
                 .collect(Collectors.toList());
     }
 
@@ -41,16 +41,22 @@ public class Node {
         return Math.pow(Math.E, - Math.pow(value - musubj, 2) / (2 * delta * delta));
     }
 
-    private double calculateOutput(double value, double weight) {
-        return value * weight;
-    }
+    /**
+     * Function to calculate the output for each [Node]
+     * Takes in a value and a weight and multiplies them
+     */
+    private Function<Double[], Double> calculateOutput = values -> values[0] * values[1];
 
-    private double linearActivation(double value) {
-        return value;
-    }
+    /**
+     * Linear Activation Function
+     * Returns the input
+     */
+    private Function<Double, Double> linearActivation = Function.identity();
 
-    private double logisticActivation(double value) {
-        return 1d / (1 + Math.pow(Math.E, -value));
-    }
+    /**
+     * Logistic Activation Function
+     * Returns the input mapped to a sigmoidal curve
+     */
+    private Function<Double, Double> logisticActivation = value -> 1d / (1 + Math.pow(Math.E, -value));
 
 }
